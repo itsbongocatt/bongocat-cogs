50ed4207a8a81a602dbfce316a957ba0ddb7d711
Bongo Cat <45111746+itsbongocatt@users.noreply.github.com>
2018-11-21 15:07:03 -0500

    Create nothing.py

---
+++ nothing/nothing.py
@@ -0,0 +1,7 @@
+import random
+from redbot.core import commands
+
+response = random.choice(["response one", "response two"])
+embed=discord.Embed()
+embed.add_field(name="Success", value=(you did nothing.)
+await self.bot.say(embed=embed)
