40ffd37fe10967403e113a7fab0d8be3ab934245
Bongo Cat <45111746+itsbongocatt@users.noreply.github.com>
2018-11-22 12:06:02 -0500

    Update fliptable.py

--- fliptable/fliptable.py
+++ fliptable/fliptable.py
@@ -7,4 +7,8 @@ class fliptable(commands.Cog):
     @commands.command()
     async def fliptable(self, ctx):
         """it flips a table"""
+        embed=discord.Embed(color=0x8080ff)
+embed.add_field(name=(╯°□°）╯︵ ┻━┻, value=You flipped a table. Your friends cheer for you., inline=False)
+embed.set_footer(text="That was pretty neat.")
+await ctx.send(embed=embed)
         await ctx.send("(╯°□°）╯︵ ┻━┻")
