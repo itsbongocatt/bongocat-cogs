b71b02fd1d2045d5e47022b74b6ebaaf4b783a43
Bongo Cat <45111746+itsbongocatt@users.noreply.github.com>
2018-11-22 01:26:05 -0500

    Delete nothing.py

--- nothing/nothing.py
+++
@@ -1,7 +0,0 @@
-import random
-from redbot.core import commands
-
-response = random.choice(["response one", "response two"])
-embed=discord.Embed()
-embed.add_field(name="Success", value=(you did nothing.)
-await self.bot.say(embed=embed)
