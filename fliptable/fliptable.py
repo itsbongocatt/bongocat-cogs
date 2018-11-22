c3d6b47c813f84add96e1d5fb2cd789adf867da5
Bongo Cat <45111746+itsbongocatt@users.noreply.github.com>
2018-11-22 11:41:48 -0500

    Delete fliptable.py

--- fliptable/fliptable.py
+++
@@ -1,4 +0,0 @@
-embed=discord.Embed(color=0x8080ff)
-embed.add_field(name=(╯°□°）╯︵ ┻━┻, value=You flipped the table., inline=False)
-embed.set_footer(text="Impressive... How'd ya do that?")
-await self.bot.say(embed=embed)
